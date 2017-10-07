8f7e02d10339e490b09c21492ac04272b2b81c5e
AlexRay <alexjamesray@gmail.com>
2017-10-07 12:27:06 -0700

    Removes dead code

--- bin/build_production.sh
+++ bin/build_production.sh
@@ -8,11 +8,6 @@ echo "building frontend"
 
 echo "copying dependent build files into backend"
 cp -R ./frontend/dist/ ./backend/dist/static/
-# cp ./frontend/dist/prerender.js ./backend/dist/
-# cp ./frontend/dist/prerender.css ./backend/dist/
-# cp ./frontend/dist/store.js ./backend/dist/
-# cp ./frontend/dist/*_manifest.json ./backend/dist/
 
 echo "building backend"
 dcprod build
-#rm ./backend/assets.json
